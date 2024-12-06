6c956f158c611230f177a327a3c1ca4651db614f
j4rl <charlie.jarl@gmail.com>
2024-12-06 10:04:02 +0100

    stuff

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        if(!isset($_POST['hela_storyn'])){
+            $story = "";
+        }
+
+        if(isset($_POST['btn'])){
+            $story = $_POST['hela_storyn'].$_POST['story'];
+        }
+    ?>
+    <h1>Story</h1>
+    <p><?=$story?></p>
+    <form action="index.php" method="POST">
+        <input type="text" name="story" placeholder="Skriv nåt...">
+        <input type="hidden" name="hela_storyn" value="<?=$story?>">
+        <input type="submit" name="btn" value="Skicka">
+    </form>
 </body>
 </html>
