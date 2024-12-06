df12ea9d8135b390e51c3d7c6f3d6ad9195af8b7
j4rl <charlie.jarl@gmail.com>
2024-12-06 09:42:16 +0100

    new slate

--- index.php
+++ index.php
@@ -6,27 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        if(isset($_GET['submit'])){
-            $fornamn = $_GET['fornamn'];
-            $efternamn = $_GET['efternamn'];
-            echo "<h1>Hej $fornamn $efternamn!</h1>";
-        }else{
-    ?>
-    <h1>Formulär</h1>
-    <form action="index.php" method="GET">
-        <input type="text" name="fornamn" placeholder="Förnamn">
-        <input type="text" name="efternamn" placeholder="Efternamn">
-        <input type="submit" name="submit" value="Skicka">
-    </form>
-    <?php   }  ?>
+    
 </body>
 </html>
-<script>
-    const forn= document.querySelector('input[name="fornamn"]');
-    forn.addEventListener('input', function(){
-        if(forn.value === "Charlie"){
-            alert("Inget fusk!");
-        }
-    });
-</script>
