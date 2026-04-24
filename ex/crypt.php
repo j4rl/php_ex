c486e9396a02429ddfcfb6070cd8653fc61a6ad0
Charlie Jarl <charlie.jarl@gmail.com>
2026-04-24 10:29:46 +0200

    Add Crypt class for AES encryption and decryption

---
+++ ex/crypt.php
@@ -0,0 +1,46 @@
+<?php
+class Crypt extends Util{
+    private $password;
+    /**
+     * Undocumented function
+     *
+     * @param string $passkey
+     */
+    function __construct($passkey="My current passkey is 100% safe!")
+    {
+        $this->password=$passkey;
+    }
+    /**
+     * Undocumented function
+     *
+     * @param [string] $plaintext
+     * @return void
+     */
+    function enc($plaintext) {    //Encrypt
+        $method="AES-256-CBC";
+        $key = hash('sha256', $this->password, true);
+        $iv = openssl_random_pseudo_bytes(16);
+        $ciphertext = openssl_encrypt($plaintext, $method, $key, OPENSSL_RAW_DATA, $iv);
+        $hash = hash_hmac('sha256', $ciphertext, $key, true);
+        $ret= $iv . $hash . $ciphertext;
+        return base64_encode($ret);
+    }
+    /**
+     * Undocumented function
+     *
+     * @param [string] $ivHashCiphertext
+     * @return void
+     */
+    function dec($ivHashCiphertext) {  //Decrypt
+        $ivHashCiphertext = base64_decode($ivHashCiphertext);
+        $method="AES-256-CBC";
+        $iv = substr($ivHashCiphertext, 0, 16);
+        $hash = substr($ivHashCiphertext, 16, 32);
+        $ciphertext = substr($ivHashCiphertext, 48);
+        $key = hash('sha256', $this->password, true);
+        if (hash_hmac('sha256', $ciphertext, $key, true) !== $hash) return null;
+        return openssl_decrypt($ciphertext, $method, $key, OPENSSL_RAW_DATA, $iv);
+    }
+}
+
+?>
